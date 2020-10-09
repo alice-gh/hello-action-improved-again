2e03483af8177f2918402b44b0504e0ef35a92c1
r <1747413+roberts-sandbox@users.noreply.github.com>
2020-10-09 00:50:46 -0500

    Update hi message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 
 try {
   const nameToGreet = core.getInput('who')
-  console.log(`Hello ${nameToGreet}`)
+  console.log(`really hello ${nameToGreet}`)
   const time = (new Date()).toTimeString()
   core.setOutput('time', time)
   const payload = JSON.stringify(github.context.payload, undefined, 2)
